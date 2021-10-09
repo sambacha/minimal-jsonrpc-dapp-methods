ec8ca7b40eab2c21387f01fe0efda8e8f03201a6
sam bacha <sam@manifoldfinance.com>
2021-10-09 03:07:43 -0700

    Create wallet_providers.js

---
+++ wallets/wallet_providers.js
@@ -0,0 +1,76 @@
+export default {
+   
+  { walletName: "coinbase", preferred: true },
+  { walletName: "trust", preferred: true, rpcUrl: RPC_URL },
+  { walletName: "metamask", preferred: true },
+  { walletName: "authereum" },
+  {
+    walletName: 'trezor',
+    appUrl: APP_URL,
+    email: CONTACT_EMAIL,
+    rpcUrl: RPC_URL
+  },
+  {
+    walletName: 'ledger',
+    rpcUrl: RPC_URL
+  },
+  {
+    walletName: 'lattice',
+    rpcUrl: RPC_URL,
+    appName: APP_NAME
+  },
+  {
+    walletName: 'keepkey',
+    rpcUrl: RPC_URL
+  },
+  {
+    walletName: 'mewwallet',
+    rpcUrl: RPC_URL
+  },
+  { 
+    walletName: 'cobovault',
+    rpcUrl: RPC_URL,
+    appName: APP_NAME,
+  },
+  { 
+    walletName: 'keystone',
+    rpcUrl: RPC_URL,
+    appName: APP_NAME,
+  },
+  {
+    walletName: "fortmatic",
+    apiKey: FORTMATIC_KEY,
+    preferred: true
+  },
+  {
+    walletName: "portis",
+    apiKey: PORTIS_KEY,
+    preferred: true,
+    label: 'Login with Email'
+  },
+  {
+    walletName: "walletConnect",
+    infuraKey: INFURA_KEY
+  },
+  { walletName: "opera" },
+  { walletName: "operaTouch" },
+  { walletName: "torus" },
+  { walletName: "status" },
+  { walletName: "walletLink" rpcUrl: RPC_URL, appName: APP_NAME }
+  { walletName: "imToken", rpcUrl: RPC_URL },
+  { walletName: "meetone" },
+  { walletName: "mykey", rpcUrl: RPC_URL },
+  { walletName: "huobiwallet", rpcUrl: RPC_URL },
+  { walletName: "hyperpay" },
+  { walletName: "wallet.io", rpcUrl: RPC_URL },
+  { walletName: "atoken" },
+  { walletName: "frame" },
+  { walletName: "ownbit" },
+  { walletName: "alphawallet" },
+  { walletName: "gnosis" },
+  { walletName: "xdefi" },
+  { walletName: "bitpie" },
+  { walletName: "binance" }
+  { walletName: "liquality" }
+  ]
+};
